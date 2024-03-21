5158a93e17b0f68b24cbfac67728b4a09272afde
OukoFranchez <oukofranchez@gmail.com>
2024-03-21 19:47:01 +0300

    refactor task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 ---ranks country origin of bands by no of fans
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
