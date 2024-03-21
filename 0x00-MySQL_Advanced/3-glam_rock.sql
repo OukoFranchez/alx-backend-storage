45dc2ff490d89391200eeef0612969f4e80bc410
OukoFranchez <oukofranchez@gmail.com>
2024-03-21 20:00:24 +0300

    refactor task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Lists bands with Glam rock as their mainstyle
-SELECT band_name, split - formed AS lifespan
-	FROM metal_bands
-	WHERE LOCATE('Glam rock', style) > 0 AND split IS NOT NULL;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
