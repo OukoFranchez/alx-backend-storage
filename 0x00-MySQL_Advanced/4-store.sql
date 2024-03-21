8aeefa9c6c49b41ee6f51ad02a2d33b2b7b33846
OukoFranchez <oukofranchez@gmail.com>
2024-03-21 20:01:11 +0300

    refactor task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,12 @@
 -- creates a trigger that decreases the quantity of an item after adding a new order
-CREATE TRIGGER remove_item
-AFTER INSERT
-ON orders
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
 BEGIN
-    SET @num_items = SELECT quantity FROM items WHERE name = NEW.item_name;
-    IF num_items > 0 THEN
-        UPDATE items SET quantity = quantity - NEW.number;
-    ELSE
-        UPDATE items SET quantity = 0;
-    END IF
-END;
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER
