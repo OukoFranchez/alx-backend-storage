0c5b980dc7e05b4e88faf5246ab97953a60e851c
OukoFranchez <oukofranchez@gmail.com>
2024-03-21 16:39:50 +0300

    add solution to task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- script that creates a table users
+-- If the table already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
