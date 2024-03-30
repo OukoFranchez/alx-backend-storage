1bd31e8dadf54123c9e558730efb6ee7d366ec59
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-30 03:05:56 +0300

    add solution to task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ python3
+"""Task 9 Module"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection
+    """
+    rslt = mongo_collection.insert_one(kwargs)
+    return rslt.inserted_id
