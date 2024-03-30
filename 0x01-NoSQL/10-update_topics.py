6437d1e38654ab71b636535cd2584d5f7afd9cd1
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-30 03:16:16 +0300

    fix the syntax error

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Task 10 Module"""
 
+
 def update_topics(mongo_collection, name, topics):
     """
     Changes all topic of collection's document based on the name
     """
     mongo_collection.update_many(
             {'name': name},
-            {'$set: {'topics': topics}'}
+            {'$seti': {'topics': topics}}
             )
